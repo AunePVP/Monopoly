9e2d21ad35082086a0897801cc4ef1aad7a8d7d1
AunePVP <67545895+AunePVP@users.noreply.github.com>
2022-04-07 13:57:44 -0700

    Deleted unused file

--- Straszen/XXVIII.php
+++
@@ -1,8 +0,0 @@
-<?php
-$cookie_name = "XXVIII";
-$cookie_value1 = "Schlossallee1";
-setcookie($cookie_name, $cookie_value1, time() + (86400 * 30), "/", "muehlhaeusler.online", 1);
-?>
-<html>
-<meta http-equiv="refresh" content="1; URL=https://muehlhaeusler.online/monopoly">
-</html>
