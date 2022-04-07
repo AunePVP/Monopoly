d5f8738effecfdab56c3c65673a696684fbdec6f
AunePVP <67545895+AunePVP@users.noreply.github.com>
2022-04-07 09:55:13 -0700

    Update index.php

--- hypothek/index.php
+++ hypothek/index.php
@@ -1,8 +1,9 @@
 <?php
 include '../config.php';
 $hname = $_GET['hname'];
+$cookie_name = $hname . "H";
 $cookie_value1 = "hypothek";
-setcookie($hname, $cookie_value1, time() + (86400 * 30), "/", $domain, 1);
+setcookie($cookie_name, $cookie_value1, time() + (86400 * 30), "/", $domain, 1);
 ?>
 <title><?php echo $hname;?></title>
 <html>
